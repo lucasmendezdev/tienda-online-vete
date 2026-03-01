2ba134436bb9cd502d638544c8c50d2e64320ca8
lucasmendezdev <lucasmendezdev@gmail.com>
2026-03-01 17:59:38 -0300

    mejora servicios

--- js/index.js
+++ js/index.js
@@ -11,3 +11,26 @@ setInterval(() => {
   slides[index].classList.add("active");
 
 }, 4000);
+
+const track = document.querySelector('.carousel-track');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let position = 0;
+const cardWidth = 300; // ancho aproximado card + gap
+
+nextBtn.addEventListener('click', () => {
+  position -= cardWidth;
+  if (Math.abs(position) > track.scrollWidth - track.clientWidth) {
+    position = 0;
+  }
+  track.style.transform = `translateX(${position}px)`;
+});
+
+prevBtn.addEventListener('click', () => {
+  position += cardWidth;
+  if (position > 0) {
+    position = -(track.scrollWidth - track.clientWidth);
+  }
+  track.style.transform = `translateX(${position}px)`;
+});
